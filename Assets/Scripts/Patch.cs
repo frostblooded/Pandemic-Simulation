a24e9c660479b63fa579e94e0f8e0060e7f2929b
frostblooded <frostblooded@yahoo.com>
2020-06-13 10:36:44 +0300

    Make patch spawn people

---
+++ Assets/Scripts/Patch.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+public class Patch : MonoBehaviour
+{
+    public int peopleCount = 16;
+    public int rowsCount = 4;
+
+    public GameObject personPrefab;
+
+    void Start()
+    {
+        float peoplePerRow = peopleCount / rowsCount;
+        
+        for(int i = 0; i < rowsCount; i++)
+        {
+            for(int j = 0; j < peoplePerRow; j++)
+            {
+                Instantiate(personPrefab, new Vector3(0.75f * j - 1.1f, -0.75f * i - 0.4f, 0), Quaternion.identity);
+            }
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+}
