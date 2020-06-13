500d6ccaa52fdf4c9362f2cd2dac186a2451cff2
frostblooded <frostblooded@yahoo.com>
2020-06-13 17:50:16 +0300

    Add day counter UI

---
+++ Assets/Scripts/DayCounter.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DayCounter : MonoBehaviour
+{
+    public Text text;
+
+    void Update()
+    {
+        text.text = "Day: " + Mathf.FloorToInt(Time.time);
+    }
+}
