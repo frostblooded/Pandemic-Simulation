d6aeaf8ade461fb9e96562ae9afcee193807d88f
frostblooded <frostblooded@yahoo.com>
2020-06-13 12:00:33 +0300

    Make bigger patches

--- Assets/Scripts/Patch.cs
+++ Assets/Scripts/Patch.cs
@@ -9,6 +9,9 @@ public class patch : MonoBehaviour
     public int peopleCount = 16;
     public int rowsCount = 4;
 
+    public float patchHeight = 3;
+    public float patchWidth = 3;
+
     public GameObject personPrefab;
     public Transform peopleHolder;
 
@@ -16,12 +19,14 @@ public class patch : MonoBehaviour
     {
         System.Random random = new System.Random();
         float peoplePerRow = peopleCount / rowsCount;
+        float distBetweenX = patchWidth / (peoplePerRow + 1);
+        float distBetweenY = -patchHeight / (rowsCount + 1);
         
         for(int i = 0; i < rowsCount; i++)
         {
             for(int j = 0; j < peoplePerRow; j++)
             {
-                Vector3 position = new Vector3(0.75f * j - 1.1f, -0.75f * i - 0.4f, 0);
+                Vector3 position = new Vector3(distBetweenX * j - patchWidth * 0.4f, distBetweenY * i - 0.4f, 0);
                 GameObject newPerson = Instantiate(personPrefab, position, Quaternion.identity);
                 newPerson.transform.SetParent(peopleHolder, false);
             }
