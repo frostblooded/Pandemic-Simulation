4f89d6221d4f96271f3413dd8cb03ad95710785b
frostblooded <frostblooded@yahoo.com>
2020-06-13 11:47:39 +0300

    Make perople walk around inside their patch

--- Assets/Scripts/Patch.cs
+++ Assets/Scripts/Patch.cs
@@ -1,24 +1,29 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
 using UnityEngine;
 
-public class Patch : MonoBehaviour
+public class patch : MonoBehaviour
 {
     public int peopleCount = 16;
     public int rowsCount = 4;
 
     public GameObject personPrefab;
+    public Transform peopleHolder;
 
     void Start()
     {
+        System.Random random = new System.Random();
         float peoplePerRow = peopleCount / rowsCount;
         
         for(int i = 0; i < rowsCount; i++)
         {
             for(int j = 0; j < peoplePerRow; j++)
             {
-                Instantiate(personPrefab, new Vector3(0.75f * j - 1.1f, -0.75f * i - 0.4f, 0), Quaternion.identity);
+                Vector3 position = new Vector3(0.75f * j - 1.1f, -0.75f * i - 0.4f, 0);
+                GameObject newPerson = Instantiate(personPrefab, position, Quaternion.identity);
+                newPerson.transform.SetParent(peopleHolder, false);
             }
         }
     }
---
+++ Assets/Scripts/Person.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Collections;
+using UnityEngine;
+
+public class Person : MonoBehaviour
+{
+    private Rigidbody2D rigidBody;
+
+    public float speed = 1;
+
+    [ReadOnly]
+    public Vector2 force;
+
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+        force = Random.insideUnitCircle.normalized;
+        rigidBody.AddForce(force * speed, ForceMode2D.Impulse);
+    }
+
+    void Update()
+    {
+    }
+}
