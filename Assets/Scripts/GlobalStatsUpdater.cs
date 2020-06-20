d682bcb180d6ba63e8c5f6268d1ddbe6669fb851
Nikolay Danailov <frostblooded@yahoo.com>
2020-06-20 11:28:44 +0300

    Add global stats to the screen-space UI

---
+++ Assets/Scripts/GlobalStatsUpdater.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GlobalStatsUpdater : MonoBehaviour
+{
+    public Text statsText;
+
+    void Update()
+    {
+        UpdateStats();
+    }
+
+    void UpdateStats()
+    {
+        List<Person> people = FindObjectsOfType<Person>().ToList();
+        int susceptibleCount = people.FindAll(p => p.infectionState == Person.InfectionState.Susceptible).Count;
+        int infectedCount = people.FindAll(p => p.infectionState == Person.InfectionState.Infected).Count;
+        int removedCount = people.FindAll(p => p.infectionState == Person.InfectionState.Removed).Count;
+
+        statsText.text = "Susceptible: " + susceptibleCount +
+                       "\nInfected: " + infectedCount +
+                       "\nRemoved: " + removedCount;
+    }
+}
